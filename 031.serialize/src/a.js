a3b8128e50d088eb5adc1559bf0a4766d2d5ddc6
skullquake <ockert8080@gmail.com>
2019-05-31 11:42:36 +0200

    .

--- 031.serialize/src/a.js
+++ 031.serialize/src/a.js
@@ -13,4 +13,11 @@ println(
 		org.apache.commons.lang3.builder.ToStringStyle.JSON_STYLE
 	)
 )
+//throws nullpointer sometimes, e.g. for req and res serialization
+println(
+	org.apache.commons.lang3.builder.ReflectionToStringBuilder.toString(
+		new java.net.URL(['http://central.maven.org/maven2/org/apache/commons/commons-lang3/3.9/commons-lang3-3.9.jar']),
+		new org.apache.commons.lang3.builder.RecursiveToStringStyle()
+	)
+);
 org.apache.commons.io.IOUtils.closeQuietly(os);
